a849d31d383f5de79c10f726edb63b1478b28ca3
Joanne <buix9830@mylaurier.ca>
2020-10-11 23:05:16 -0400

    Finished part 3

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "MainActivity";
+    static final int requestCode = 10;
+    int resposeCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(ACTIVITY_NAME, "In onCreate()");
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
+
     protected void onStart(){
         super.onStart();
         Log.i(ACTIVITY_NAME, "In onStart()");
@@ -33,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+    public void button(){
+        Button buttonText = (Button)findViewById(R.id.button);
+        Intent intent = new Intent(this, ListItemsActivity.class);
+        startActivityForResult(intent, requestCode);
+
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data){
+        if ((requestCode == 10) && (resposeCode == Activity.RESULT_OK)){
+            Log.i(ACTIVITY_NAME, "returned to MainActivity.onActivityResult");
+            String messagePassed = data.getStringExtra("Response");
+            CharSequence text = "ListItemsActivity passed: " + messagePassed;
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }
     }
 
 
